d7b90cf4112b747684771449da6f9af82216d76d
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-10-22 15:35:07 +0600

    banner section added

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,2 +1,3 @@
 export { default as logo } from "./logo.png"
 export { default as user } from "./user.png"
+export { default as macaroons } from "./macaroons.png"
---
+++ src/sections/Banner/Banner.jsx
@@ -0,0 +1,22 @@
+import { macaroons } from "../../assets"
+
+const Banner = () => {
+  return (
+    <section 
+        id="banner"
+        className="bg-cover bg-center bg-no-repeat"
+        style={{backgroundImage: `url(${macaroons})`}}
+    >
+        {/* banner container */}
+        <div className="container px-6 py-[60px] md:py-[80px] lg:py-[100px] xl:py-[120px]">
+            {/* banner heading */}
+            <h1 className="w-[150px] font-sail font-normal text-[40px] text-mediumLightPink sm:w-[250px] md:w-[300px] md:text-[50px] lg:w-[400px] lg:text-[70px] xl:w-[650px] xl:text-[110px]">
+                Sweet Treats 
+                for the 
+                sweet tooth
+            </h1>
+        </div>
+    </section>
+  )
+}
+export default Banner
