fb7ee5237114ac988b0c35a9a6f2d44c01f1c147
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-10-23 17:59:45 +0600

    banner minor modification

--- src/sections/Banner/Banner.jsx
+++ src/sections/Banner/Banner.jsx
@@ -8,9 +8,13 @@ const Banner = () => {
         style={{backgroundImage: `url(${macaroons})`}}
     >
         {/* banner container */}
-        <div className="container px-6 py-[60px] md:py-[80px] lg:py-[100px] xl:py-[120px]">
+        <div className="container px-6 py-[60px] md:py-[80px] 
+        lg:py-[100px] xl:py-[120px]">
             {/* banner heading */}
-            <h1 className="w-[150px] font-sail font-normal text-[40px] text-mediumLightPink sm:w-[250px] md:w-[300px] md:text-[50px] lg:w-[400px] lg:text-[70px] xl:w-[650px] xl:text-[110px]">
+            <h1 className="w-[150px] font-sail font-normal 
+            text-[40px] text-mediumLightPink sm:w-[250px] 
+            md:w-[300px] md:text-[50px] lg:w-[400px] 
+            lg:text-[70px] xl:w-[650px] xl:text-[110px]">
                 Sweet Treats 
                 for the 
                 sweet tooth
