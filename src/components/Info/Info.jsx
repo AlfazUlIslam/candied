349d6c86d9325fa3e40fdf969ff523488004cc3b
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-11-04 14:58:08 +0600

    contact us desktop styles added

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -3,22 +3,21 @@ const Info = (props) => {
         
     return (
         // info
-        // gap-[30px] 
         <div className="flex justify-start items-center 
-        gap-[10px]">
+        gap-[10px] sm:gap-[30px] md:gap-[10px] xl:gap-[30px]">
             {/* icon */}
             <div>
 
             <img
-                className="w-[20px]"
+                className="w-[20px] sm:w-auto md:w-[20px]]"
                 src={image} 
                 alt={altText} 
             />
             </div>
             {/* info text */}
-            {/* w-[340px] text-[18px] */}
             <p className="w-[150px] font-poppins font-normal 
-            text-[16px] text-darkPink">
+            text-[16px] text-darkPink sm:w-[340px] sm:text-[18px] 
+            md:w-[250px] md:text-[16px] xl:w-[340px] xl:text-[18px]">
                 {infoText}
             </p>
         </div>
--- src/sections/ContactUs/ContactUs.jsx
+++ src/sections/ContactUs/ContactUs.jsx
@@ -6,39 +6,43 @@ const ContactUs = () => {
     // contact us
     <section id="contact-us">
         {/* contact us container */}
-        {/* py-[120px] flex-row */}
-        <div className="container px-6 py-[60px] 
-        flex flex-col justify-center items-center gap-[98px]">
+        <div className="container px-6 py-[60px] flex flex-col 
+        justify-center items-center gap-[98px] md:flex-row 
+        md:gap-[50px] xl:py-[120px]">
             {/* image */}
             <div>
                 <img 
+                    className="w-[300px] sm:w-[500px] 
+                    md:w-[300px] lg:w-[400px] xl:w-auto"
                     src={contactUsImage} 
                     alt="Contact Us Image" 
                 />
             </div>
             {/* content */}
-            {/* items-start */}
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center 
+            md:items-start">
                 {/* title */}
-                {/* text-[36px] */}
                 <h3 className="font-poppins font-semibold 
                 text-[22px] text-lightBrownishMaroon 
-                text-center">
+                text-center sm:text-[36px] md:text-[22px] 
+                lg:text-[26px] xl:text-[36px]">
                     Contact Us
                 </h3>
                 {/* heading */}
-                {/* w-[483px] mt-[32px] mb-[25px] text-[64px] */}
                 <h2 className="w-auto mt-[22px] mb-[15px] 
                 font-sail font-normal text-[44px] 
-                text-lightBrownishMaroon text-center">
+                text-lightBrownishMaroon text-center 
+                sm:w-[483px] sm:text-[64px] md:w-auto 
+                md:text-[44px] lg:text-[64px] xl:w-[483px] 
+                xl:mt-[32px] xl:mb-[25px]">
                     <span className="mr-4 text-mediumLightPink">
                         Sweeten
                     </span>
                     your day!
                 </h2>
                 {/* infos */}
-                {/* mb-[100px] */}
-                <div className="mb-[50px] space-y-[34px]">
+                <div className="mb-[50px] space-y-[34px] 
+                xl:mb-[100px]">
                     <Info 
                         image={clockIcon}
                         altText={"Clock Icon"}
@@ -51,14 +55,17 @@ const ContactUs = () => {
                     />
                 </div>
                 {/* button */}
-                {/* w-[178px] h-[54px] rounded-tr-[100px] 
-                rounded-bl-[100px] text-[18px] */}
                 <button className="w-[158px] h-[44px] 
                 rounded-tr-[40px] rounded-bl-[40px] 
                 bg-mediumLightPink font-poppins font-normal 
                 text-[16px] text-[#FFF] flex justify-center 
                 items-center transition-all duration-1000 
-                hover:bg-darkPink active:scale-90">
+                hover:bg-darkPink active:scale-90 sm:w-[178px] 
+                sm:h-[54px] sm:rounded-tr-[100px] sm:rounded-bl-[100px] 
+                sm:text-[18px] md:w-[158px] md:h-[44px] md:rounded-tr-[40px] 
+                md:rounded-bl-[40px] md:text-[16px] xl:w-[178px] 
+                xl:h-[54px] xl:rounded-tr-[100px] xl:rounded-bl-[100px] 
+                xl:text-[18px]">
                     Contact
                 </button>
             </div>
